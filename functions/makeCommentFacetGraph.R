20d7945e6942ced545bab244d4854fa500063423
AlejandroKantor <akantorb@gmail.com>
2017-01-18 13:03:50 -0500

    Use theme function. Move legend to bottom.

--- functions/makeCommentFacetGraph.R
+++ functions/makeCommentFacetGraph.R
@@ -33,18 +33,7 @@ makeCommentFacetGraph <- function( plot.data ){
     labs(x="Original Poster Ideology",y="Post-Comment Pairs") +
     scale_x_discrete(breaks=NULL) +
     scale_fill_discrete(name="Commenter Ideology") +
-    theme(legend.justification=legend.place[1,], legend.position=legend.place[2,], 
-          plot.title = element_text(hjust = 0.5, face="bold", size=20),
-          axis.title = element_text(face="bold", size=20),
-          legend.title = element_text(face="bold", size=14),
-          legend.text = element_text(size=15),
-          legend.background = element_rect(fill="white", size=1, linetype="solid",colour="black"),
-          legend.key.size = unit(0.5, "cm"),
-          strip.text.x = element_text(size=15, face="bold"),
-          panel.grid.major = element_line(colour = "grey10"),
-          panel.grid.minor = element_line(colour = "white"),
-          panel.background = element_rect(fill = "white"),
-          panel.border = element_rect(colour = "black", fill=NA, size=2),
-          text = element_text(family="Times"))
+    getTheme()
+
   return(g_plot)
 }
--- functions/makeUpvoteFacetGraph.R
+++ functions/makeUpvoteFacetGraph.R
@@ -38,18 +38,6 @@ makeUpvoteFacetGraph <- function( plot.data){
     labs(x="Original Poster Ideology",y="Upvote-Post Pairs") +
     scale_x_discrete(breaks=NULL) +
     scale_fill_discrete(name="Upvoter Ideology") +
-    theme(legend.justification=legend.place[1,], legend.position=legend.place[2,], 
-          plot.title = element_text(hjust = 0.5, face="bold", size=20),
-          axis.title = element_text(face="bold", size=20),
-          legend.title = element_text(face="bold", size=14),
-          legend.text = element_text(size=15),
-          legend.background = element_rect(fill="white", size=1, linetype="solid",colour="black"),
-          legend.key.size = unit(0.5, "cm"),
-          strip.text.x = element_text(size=15, face="bold"),
-          panel.grid.major = element_line(colour = "grey10"),
-          panel.grid.minor = element_line(colour = "white"),
-          panel.background = element_rect(fill = "white"),
-          panel.border = element_rect(colour = "black", fill=NA, size=2),
-          text = element_text(family="Times"))
+    getTheme() 
   return(g_upvote_graph)
 }
