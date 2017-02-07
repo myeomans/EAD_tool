2e62a672617a4709c08f699855cd94d8f99988dd
myeomans <mk.yeomans@gmail.com>
2017-02-07 18:05:17 -0500

    intake

--- functions/data_intake.R
+++ functions/data_intake.R
@@ -1,12 +1,14 @@
 
-data.checker<-function(posts_x, forum_x, enrol_x, pc_x, users_x,
+data.checker<-function(posts_x, forum_x, enrol_x, pc_x, users_x, of.interest,
                        USA.only=TRUE, self.posts=FALSE, course.threads.only=TRUE,
                        course.name="Course", start.date=NULL, course.team.names=NULL){
   if((sum(colnames(users_x)[1:10]==paste0("V",1:10))==10)){
     colnames(users_x)[1:10]<-as.character(unlist(users_x[1,1:10]))
     users_x<-users_x[2:nrow(users_x),]
   }
-  # Descriptives from Sub-Populations
+  ###############################################################################################
+  # User-Level Details
+  ###############################################################################################
   table_build<-list()
   
   # Enrolled
@@ -23,8 +25,10 @@ data.checker<-function(posts_x, forum_x, enrol_x, pc_x, users_x,
                  by="id_map_hash_id", all.x=F)
   table_build[["surveyed"]]<-table_row_build(users_x, start.date)
   
-  # Ideology Response
-  users_x$leftright<-as.numeric(substr(users_x$wvsurvey_1,0,2))
+  # Covariate Of Interest Response
+  users_x$leftright<-ifelse(of.interest=="wvsurvey_1",
+                            as.numeric(substr(users_x$wvsurvey_1,0,2)),
+                            users_x[,of.interest])
   users_x<-users_x[!is.na(users_x$leftright),]
   users_x<-users_x[!duplicated(users_x$user_id),]
   table_build[["ideology"]]<-table_row_build(users_x, start.date)
@@ -34,13 +38,11 @@ data.checker<-function(posts_x, forum_x, enrol_x, pc_x, users_x,
     users_x<-users_x[(users_x$profile_country=="US"),]
     table_build[["ideology_USA"]]<-table_row_build(users_x, start.date)
   }
-  
+  ###############################################################################################
+  # Forum Structure
+  ###############################################################################################
   posts_x<-post_process(posts_x, start.date)
   posts_x<-merge(posts_x,users_x[,c("user_id","leftright")],by="user_id", all.x=T)
-  
-  ###############################################################################################
-  # Forum Structure Labelling
-  ###############################################################################################
   posts_x[,c("toppost_name","parent_name")]<-""
   posts_x[,c("toppost_span","toppost_leftright","parent_span","parent_leftright","comment_order")]<-NA
   
@@ -55,6 +57,9 @@ data.checker<-function(posts_x, forum_x, enrol_x, pc_x, users_x,
       try(posts_x[x,]$comment_order<-which(rev(posts_x[posts_x$parent_ids==posts_x[x,]$parent_ids,"mongoid"])==posts_x[x,]$mongoid), silent=T)
     }
   }
+  posts_x$course.post<-1*(posts_x$author_username%in%course.team.names)
+  posts_x$self.reply<-1*((posts_x$parent_name==posts_x$author_username)|(posts_x$toppost_name==posts_x$author_username))
+  posts_x$admin<-posts_x$issue<-0
   ###############################################################################################
   # Upvotes!
   ###############################################################################################
@@ -70,27 +75,8 @@ data.checker<-function(posts_x, forum_x, enrol_x, pc_x, users_x,
   upvotes_x$poster_leftright<-as.numeric(upvotes_x$poster_leftright)
   upvotes_x$upvote_leftright<-as.numeric(upvotes_x$upvote_leftright)
   
-  ###############################################################################################
-  # Thread-level data
-  ###############################################################################################
-  threads_x<-posts_x[posts_x$level==1,]
-  threads_x[,paste0(c("reply","comment","upvote"),"_count")]<-NA
-  for (x in 1:nrow(threads_x)){
-    threads_x[x,"reply_count"]<-sum((posts_x$thread_id==threads_x[x,"mongoid"])&(posts_x$level==2))
-    threads_x[x,"comment_count"]<-sum((posts_x$thread_id==threads_x[x,"mongoid"])&(posts_x$level==3))
-    threads_x[x,"upvote_count"]<-sum((upvotes_x$thread_id==threads_x[x,"mongoid"]))
-  }
-  threads_x$activities<-rowSums(threads_x[,paste0(c("reply","comment","upvote"),"_count")])
-  ###############################################################################################
-  # Thread Types
-  ###############################################################################################
-  threads_x$course.post<-1*(threads_x$author_username%in%course.team.names)
-  posts_x$course.post<-1*(posts_x$thread_id%in%threads_x[threads_x$course.post==1,"thread_id"])
-  
-  posts_x$self.reply<-1*((posts_x$parent_name==posts_x$author_username)|(posts_x$toppost_name==posts_x$author_username))
-  posts_x$admin<-posts_x$issue<-0
+  upvotes_x$course.post<-1*(upvotes_x$thread_id%in%posts_x[(posts_x$course.post==1)&(posts_x$level==1),"thread_id"])
   upvotes_x$admin<-upvotes_x$issue<-0
-  
   ###############################################################################################
   # Activity Counts per person
   ###############################################################################################
@@ -104,5 +90,6 @@ data.checker<-function(posts_x, forum_x, enrol_x, pc_x, users_x,
     users_x[x,"upvote_count"]<-sum(FOCup_x&(upvotes_x$upvote_username==users_x[x,"username"]),na.rm=T)
   }
   users_x$activities<-rowSums(users_x[,paste0(c("reply","comment","upvote"),"_count")])
+  ###############################################################################################
   table_build[["forum_active"]]<-table_row_build(users_x[users_x$activities>0,], start.date)
 }
