a7b127f09c8176ca50d44e282baf44dcef2b49e0
AlejandroKantor <akantorb@gmail.com>
2017-02-15 16:25:36 -0500

    Use fread for data input

--- shinyApp/functions/data_functions.R
+++ shinyApp/functions/data_functions.R
@@ -17,7 +17,9 @@ dfFromCsv <- function(input, s_file_name){
   if (is.null(l_in_file)) {
     return(NULL) 
   } else {
-    df_data <- read.csv(l_in_file$datapath, stringsAsFactors = F)
+    #import using fread since it is quicker
+    dt_data <- fread(input = l_in_file$datapath)
+    df_data <- data.frame(dt_data)
     return(df_data)
   }
 }
