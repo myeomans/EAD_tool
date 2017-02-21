f70dc1125d537b8f62bffbb1324b94f2f55ff34a
myeomans <mk.yeomans@gmail.com>
2017-02-21 11:45:16 -0500

    sleek inputs

--- shinyApp/functions/makeIdeologyGraph.R
+++ shinyApp/functions/makeIdeologyGraph.R
@@ -8,9 +8,9 @@ makeIdeologyGraph <- function(user.data, post.data, settings){
   user.data<-user.data[!is.na(user.data[,settings$of.interest]),]
   if(settings$usa.only){user.data<-user.data[user.data$USA==1,]}
   if ((!is.null(post.data))&settings$forum.active){
-    if(settings$course.only){
-      post.data<-post.data[post.data$course.post==1,]
-    }
+    # if(settings$course.only){
+    #   post.data<-post.data[post.data$course.post==1,]
+    # }
     user.data<-user.data[unlist(sapply(1:nrow(user.data),function(x) sum((post.data$user_id==user.data[x,"user_id"]))))>0,]
   }
   if(length(unique(user.data[,settings$of.interest]))<12){
