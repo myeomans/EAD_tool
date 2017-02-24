c7f3946b77954374e9123b7135a7be18f02071b2
AlejandroKantor <akantorb@gmail.com>
2017-02-24 16:10:29 -0500

    Generalized, uses pre calculated post data

--- shinyApp/functions/makeCommentFacetGraph.R
+++ shinyApp/functions/makeCommentFacetGraph.R
@@ -10,39 +10,43 @@
 #table(posts_hks$TriPart,posts_hks$parent_TriPart)
 ##################################################################
 
-makeCommentFacetGraph <- function(user.data, post.data, settings){  
-  if( is.null(user.data) | is.null(post.data)){
+
+makeCommentFacetGraph <- function( post.data, settings){  
+  if( is.null(post.data)){
     return(NULL)
   }
   ##################################################################
   # Merge in relevant dimension
-  TOP<-paste0("parent_",settings$of.interest)
-  BOT<-settings$of.interest
-  parent.user.data<-user.data
-  parent.user.data[,paste0("parent_",c("user_id",BOT))]<-parent.user.data[,c("user_id",BOT)]
-  post.data<-merge(post.data,user.data[,c("user_id",BOT)],by="user_id",all.x=T)
-  post.data<-merge(post.data,parent.user.data[,c("parent_user_id",TOP)],by="parent_user_id",all.x=T)
-  post.data$top<-post.data[,TOP]
-  post.data$bot<-post.data[,BOT]
-  ##################################################################
-  # Filter posts
-  exclusions<-(post.data$bot!="")&(post.data$top!="")&(post.data$level>2)
-  exclusions<-exclusions&(!is.na(post.data$bot))&(!is.na(post.data$top))
-  # if(settings$course.only) exclusions<-exclusions&(post.data$course.post==1)
-  # if(!settings$self.posts)  exclusions<-exclusions&(post.data$self.reply==0)
-  if(settings$usa.only){
-    exclusions<-exclusions&(post.data$user_id%in%user.data[user.data$USA==1,"user_id"])
-    exclusions<-exclusions&(post.data$parent_user_id%in%user.data[user.data$USA==1,"user_id"])
-  }
-  #if(settings$short.yes)   exclusions<-exclusions&(post.data$shortyes==0)
 
-  post.data<-post.data[exclusions,]
+  s_of_interest <- settings$of.interest
+  s_of_interest_parent <- paste0("parent_",s_of_interest)
+  
+  post.data$bot <- post.data[[s_of_interest]]
+  post.data$top <- post.data[[s_of_interest_parent]]
+ 
+  if(!is.factor(post.data$bot)){
+    post.data$bot <- factor(post.data$bot)
+    post.data$top <- factor(post.data$top)
+  }
+   
   ##################################################################
-  plot.top <- max(table(post.data$bot,post.data$top))
+  # # Filter posts
+  valid_rows <- (post.data$bot!="") & (post.data$top!="") & (post.data$level>2)
+  valid_rows <- valid_rows&(!is.na(post.data$bot)) & (!is.na(post.data$top))
+  #if(settings$course.only) valid_rows <- valid_rows & (post.data$course.post==1)
+  #if(!settings$self.posts)  valid_rows <- valid_rows & (post.data$self.reply==0)
+  if(settings$usa.only){
+    valid_rows<-valid_rows&(post.data$USA==1)
+    valid_rows<-valid_rows&(post.data$parent_USA==1)
+  }
+  #if(settings$short.yes)   valid_rows<-valid_rows&(post.data$shortyes==0)
+
+  post.data<-post.data[valid_rows,]
+  ##################################################################
   
   g_plot <- qplot(bot, data=post.data, geom="bar", fill=bot) +
     facet_wrap(~ top, strip.position = "bottom") + 
-    ylim(0,plot.top) + scale_fill_brewer( palette = "Set1")+
+    ylim(0,NA) + scale_fill_brewer( palette = "Set1")+
     labs(x="Original Poster Ideology",y="Post-Comment Pairs", fill = "Commenter Ideology") +
     scale_x_discrete(breaks=NULL) +
     #scale_fill_discrete(name="Commenter Ideology") +
