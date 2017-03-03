ed1c68ad75855812623d96024aa64b46b0eb8882
myeomans <mk.yeomans@gmail.com>
2017-03-03 13:46:34 -0500

    new files

--- shinyApp/functions/plotHeatMap.R
+++ shinyApp/functions/plotHeatMap.R
@@ -14,17 +14,16 @@ plotHeatMap <- function(post.data, settings){
   
   if(!is.factor(post.data$bot)){
     if(length(levels(as.factor(post.data$bot)))>7){
+      buckets<-quantile(post.data$bot, probs=seq(0,1, by=0.25), na.rm=TRUE)
       
-      
-      post.data$bot<-cut(post.data$bot, breaks=quantile(post.data$bot, probs=seq(0,1, by=0.25), na.rm=TRUE), 
-                    include.lowest=TRUE, ordered_result=T,dig.lab = 2)
-      post.data$top<-cut(post.data$top, breaks=quantile(post.data$bot, probs=seq(0,1, by=0.25), na.rm=TRUE), 
-                    include.lowest=TRUE, ordered_result=T,dig.lab = 2)
+      post.data$bot<-cut(post.data$bot, breaks=buckets, include.lowest=TRUE, ordered_result=T,dig.lab = 2)
+      post.data$top<-cut(post.data$top, breaks=buckets, include.lowest=TRUE, ordered_result=T,dig.lab = 2)
     }else{
       post.data$bot <- factor(post.data$bot)
       post.data$top <- factor(post.data$top)
     }
-  }
+  } 
+  s_f_levels <- levels(post.data$bot)
   
   post.data <- data.table(post.data)
   
@@ -46,6 +45,10 @@ plotHeatMap <- function(post.data, settings){
   dt_comb <- CJ(v_levels, v_levels)
   
   v_s_names <- c("user_ideology", "respond_ideology")
+  if(exists("s_f_levels")){
+    dt_comb[ , user_ideology := factor(user_ideology , levels = s_f_levels)]
+  }
+  
   setnames(dt_freq , v_s_cols, v_s_names)
   setnames(dt_comb  , v_s_names)
   dt_freq <- merge(dt_comb, dt_freq, by = v_s_names, all.x  = TRUE)
@@ -62,7 +65,7 @@ plotHeatMap <- function(post.data, settings){
     dt_freq[ , user_ideology := factor(user_ideology, v_levels)] 
     dt_freq[ , respond_ideology := factor(respond_ideology, levels=rev(v_levels))]
   } else {
-    dt_freq[ , respond_ideology := factor(respond_ideology, levels=rev(levels(respond_ideology)))]
+    dt_freq[ , respond_ideology := factor(respond_ideology, levels=s_f_levels)]
   }
   
   # original code incorrectly set x as response ideology but xlab as user ideology
