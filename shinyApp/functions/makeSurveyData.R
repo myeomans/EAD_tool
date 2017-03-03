2a5838aa1425ca6dacde52d8fa109761e8a131e4
AlejandroKantor <akantorb@gmail.com>
2017-03-03 14:07:32 -0500

    Make count 0 if NAs

--- shinyApp/functions/makeSurveyData.R
+++ shinyApp/functions/makeSurveyData.R
@@ -15,9 +15,14 @@ makeSurveyData <- function(df_survey = NULL, df_post = NULL, df_upvote=NULL, set
     df_count_replies <- makeCounts(df_post[df_post$level==2,],"user_id", "count_replies")
     df_count_comments <- makeCounts(df_post[df_post$level>2,],"user_id", "count_comments")
     
-    df_survey <- merge( df_survey, df_count_post, by = "user_id", all.x=TRUE)
+    df_survey <- merge( df_survey, df_count_post, by = "user_id", all.x=TRUE,)
     df_survey <- merge( df_survey, df_count_replies, by = "user_id", all.x=TRUE)
     df_survey <- merge( df_survey, df_count_comments, by = "user_id", all.x=TRUE)
+    
+    df_survey[ is.na(df_survey$count_post) , "count_post"] <- 0L
+    df_survey[ is.na(df_survey$count_replies) , "count_replies"] <- 0L
+    df_survey[ is.na(df_survey$count_comments) , "count_comments"] <- 0L
+    
   }
   
   # add components from df_upvote
@@ -26,6 +31,7 @@ makeSurveyData <- function(df_survey = NULL, df_post = NULL, df_upvote=NULL, set
     df_count_upvotes <- makeCounts(df_upvote,"user_id", "count_upvotes")
     
     df_survey <- merge( df_survey, df_count_upvotes, by = "user_id", all.x=TRUE)
+    df_survey[ is.na(df_survey$count_upvotes) , "count_upvotes"] <- 0L
   }
   
   return(df_survey)
